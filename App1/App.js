ae71c578a086ca5348ada3e83ebf7b402f7ed13a
Hari Praneeth V <vhpraneeth2001@gmail.com>
2022-11-28 08:30:25 +0000

    Updated

--- App1/App.js
+++ App1/App.js
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [myVar, setMyVar] = useState('Hello World');
   useEffect(() => {
     setMyVar('Hello World 2');
   }, []);
+  const buttonClick = () => {
+    setMyVar('Hello World 3');
+    alert(myVar);
+  };
   return (
     <View style={styles.container}>
-      <Text> {myVar} </Text>
+      <TouchableOpacity onPress={buttonClick}>
+        <Text>{myVar}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
